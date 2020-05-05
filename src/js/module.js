037f9ab2d00549e598b09fc095b0b1ac81546b76
Starinets <tsn.ipad@gmail.com>
2020-05-06 03:21:34 +0600

    function addTask

--- src/js/module.js
+++ src/js/module.js
@@ -1,3 +1,32 @@
 {
-    console.log('Hello Lectrum!');
+  const form = document.querySelector('.createTaskForm');
+
+  form.onsubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const task = formData.get('task');
+
+    addTask(task);
+  };
+
+  const getUpdatedTemplate = (task) => `
+  <div class="content">
+    <button class="checkbox"></button>
+    <input class="taskInput" type="text" value="${task}" disabled />
+  </div>
+  <div class="actions">
+    <button class="star"></button>
+    <button class="edit"></button>
+    <button class="remove"></button>
+  </div>
+  `;
+
+  const addTask = (task) => {
+    const tasksDOM = document.querySelector('.tasks');
+    const taskDOM = document.createElement('li');
+
+    taskDOM.innerHTML = getUpdatedTemplate(task);
+    tasksDOM.prepend(taskDOM);
+  };
 }
