cce15ab3a03c082abc9863382f313c04531b23d4
Starinets <tsn.ipad@gmail.com>
2020-05-12 04:03:07 +0600

    base styles for task (ul>li)

--- src/js/module.js
+++ src/js/module.js
@@ -12,19 +12,19 @@ form.onsubmit = (event) => {
 };
 
 const getUpdatedTemplate = (task) => `
-  <div class="tasks-list__task-content">
-    <button class="tasks-list__task-checkbox"></button>
-    <input class="tasks-list__task-name" type="text" value="${task}" disabled />
+  <div class="task__content">
+    <button class="task__checkbox"></button>
+    <input class="task__name" type="text" value="${task}" disabled />
   </div>
-  <div class="tasks-list__task-actions">
-    <button class="tasks-list__task-star"></button>
-    <button class="tasks-list__task-edit"></button>
-    <button class="tasks-list__task-remove"></button>
+  <div class="task__actions">
+    <button class="task__star"></button>
+    <button class="task__edit"></button>
+    <button class="task__remove"></button>
   </div>
   `;
 
 const addFavoriteHandler = (taskDOM) => {
-  const starDOM = taskDOM.querySelector('.tasks-list__task-star');
+  const starDOM = taskDOM.querySelector('.task__star');
 
   starDOM.onclick = () => {
     starDOM.classList.toggle('selected');
@@ -32,10 +32,10 @@ const addFavoriteHandler = (taskDOM) => {
 };
 
 const addEditHandler = (taskDOM) => {
-  const editDOM = taskDOM.querySelector('.tasks-list__task-edit');
+  const editDOM = taskDOM.querySelector('.task__edit');
 
   editDOM.onclick = () => {
-    const taskInputDOM = taskDOM.querySelector('.tasks-list__task-name');
+    const taskInputDOM = taskDOM.querySelector('.task__name');
     const isDisabled = taskInputDOM.getAttribute('disabled') === null;
 
     if (isDisabled) {
@@ -47,7 +47,7 @@ const addEditHandler = (taskDOM) => {
 };
 
 const addRemoveHandler = (taskDOM) => {
-  const removeDOM = taskDOM.querySelector('.tasks-list__task-remove');
+  const removeDOM = taskDOM.querySelector('.task__remove');
 
   removeDOM.onclick = () => taskDOM.remove();
 };
@@ -55,6 +55,8 @@ const addRemoveHandler = (taskDOM) => {
 const addTask = (task) => {
   const tasksDOM = document.querySelector('.tasks-list');
   const taskDOM = document.createElement('li');
+  taskDOM.classList.add('task-list_task');
+  taskDOM.classList.add('task');
 
   taskDOM.innerHTML = getUpdatedTemplate(task);
   tasksDOM.prepend(taskDOM);
